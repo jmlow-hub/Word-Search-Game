c4a0020794fffd8d27e8c8080b164e865800ac91
Jackie Low <jmlow74@gmail.com>
2021-06-29 21:50:04 +0100

    for loop to generate random list of words for game

--- app.dev.js
+++ app.dev.js
@@ -7,6 +7,14 @@ var wordList = []; //fetch list of words from API
 
 var rawWordsArr = fetch("https://api.datamuse.com/words?ml=software+development").then(function (res) {
   return res.json();
-}).then(function (data) {})["catch"](function (err) {
+}).then(function (data) {
+  //pull 10 words between 3 and 6 words in length from list at random and pushes to wordList array
+  for (var i = 0; i < 30; i++) {
+    var index = Math.floor(Math.random() * 100);
+    if (data[index].word.length > 2 && data[index].word.length < 7) wordList.push(data[index].word);
+    console.log(wordList);
+  } //how to ensure get minimum amount of words - increase i?
+
+})["catch"](function (err) {
   alert("You've rendered us speechless, we're all out of words");
 });
--- app.js
+++ app.js
@@ -10,16 +10,22 @@ const wordList = [];
   return res.json();
 })
 .then(data => {
- 
 
+ //pull 10 words between 3 and 6 words in length from list at random and pushes to wordList array
+  for(let i=0; i < 30; i++) {
+  const index = Math.floor(Math.random() *100);
+  if(data[index].word.length > 2 && data[index].word.length < 7)
+  wordList.push(data[index].word);
+  console.log(wordList);
+  
+}
+//how to ensure get minimum amount of words - increase i?
+
+  
 })
-
 .catch(err => {
   alert("You've rendered us speechless, we're all out of words")
 })
-
-
-
 
 
 
