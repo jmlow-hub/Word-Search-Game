5954202d22a570e963af96988c51afde71b7698a
Jackie Low <jmlow74@gmail.com>
2021-06-30 14:43:06 +0100

    do..while loop set up to create array of words to use in game

--- app.dev.js
+++ app.dev.js
@@ -9,12 +9,26 @@ var rawWordsArr = fetch("https://api.datamuse.com/words?ml=software+development"
   return res.json();
 }).then(function (data) {
   //pull 10 words between 3 and 6 words in length from list at random and pushes to wordList array
-  for (var i = 0; i < 30; i++) {
+  //how to ensure unique words - includes()?
+  var i = 0;
+
+  do {
     var index = Math.floor(Math.random() * 100);
-    if (data[index].word.length > 2 && data[index].word.length < 7) wordList.push(data[index].word);
+    if (data[index].word.length > 2 && data[index].word.length < 7 && !wordList.includes(i)) //hopefully !includes() means won't return duplicates
+      wordList.push(data[index].word);
     console.log(wordList);
-  } //how to ensure get minimum amount of words - increase i?
+  } while (wordList.length < 6); // for(let i=0; i < 30; i++) {
+  // const index = Math.floor(Math.random() *100);
+  // if(data[index].word.length > 2 && data[index].word.length < 7 && !wordList.includes(i)) //hopefully !includes() means won't return duplicates
+  // wordList.push(data[index].word);
+  // console.log(wordList);
+  //how to ensure get minimum amount of words - increase i? or stop when get to right amount?
 
 })["catch"](function (err) {
   alert("You've rendered us speechless, we're all out of words");
-});
+}); //create list of words to find in relevant container
+//create 10 x 10 grid in relevant container
+//place words from array in grid
+//fill empty spaces with random letters
+//select word - change color - first letter last letter?
+//word is removed or indicated as clicked on list somehow
--- app.js
+++ app.js
@@ -12,14 +12,30 @@ const wordList = [];
 .then(data => {
 
  //pull 10 words between 3 and 6 words in length from list at random and pushes to wordList array
-  for(let i=0; i < 30; i++) {
+ //how to ensure unique words - includes()?
+ let i = 0;
+ do {
   const index = Math.floor(Math.random() *100);
-  if(data[index].word.length > 2 && data[index].word.length < 7)
+  if(data[index].word.length > 2 && data[index].word.length < 7 && !wordList.includes(i)) //hopefully !includes() means won't return duplicates
   wordList.push(data[index].word);
-  console.log(wordList);
+  console.log(wordList)
+ } while (wordList.length < 6);
+
+
+ 
+
+
+
+
+  // for(let i=0; i < 30; i++) {
+  // const index = Math.floor(Math.random() *100);
+  // if(data[index].word.length > 2 && data[index].word.length < 7 && !wordList.includes(i)) //hopefully !includes() means won't return duplicates
+  // wordList.push(data[index].word);
+  // console.log(wordList);
   
-}
-//how to ensure get minimum amount of words - increase i?
+
+//how to ensure get minimum amount of words - increase i? or stop when get to right amount?
+
 
   
 })
@@ -29,3 +45,10 @@ const wordList = [];
 
 
 
+//create list of words to find in relevant container
+
+//create 10 x 10 grid in relevant container
+//place words from array in grid
+//fill empty spaces with random letters
+//select word - change color - first letter last letter?
+//word is removed or indicated as clicked on list somehow
